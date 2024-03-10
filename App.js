6fcdd28acbca4cfef0ca60e9bf4bebf2763db8b1
ridemountainpig <lin0905580802@gmail.com>
2024-03-10 17:10:50 +0800

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View className="flex-1 items-center justify-center bg-black">
+            <Text className="text-xl font-semibold text-white">
+                React Native With TailwindCss Template
+            </Text>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ README.md
@@ -0,0 +1,35 @@
+# React Native TailwindCSS Template
+
+React Native template with JavaScript and Tailwind.
+
+## Getting Started
+
+You will need:
+
+- [Node.js 18+](https://nodejs.org/en/).
+
+1. Install dependencies:
+
+```bash
+# Install with npm:
+npm install
+
+# Install with pnpm:
+pnpm install
+
+# Install with yarn:
+yarn install
+```
+
+2. Start the development server
+
+```bash
+# Start with npm:
+npm run start
+
+# Start with pnpm:
+pnpm start
+
+# Start with yarn:
+yarn start
+```
